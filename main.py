8e7b55d11385092fc446b6ed4ca88a255b8fd1b9
Lucky  Odi <lucky.odi@openaccessmetro.net>
2025-09-05 13:24:43 +0100

    Add login and register functionalities alongside templates

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime, timezone
 from dotenv import load_dotenv
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 from flask_login import login_required, login_user, current_user, LoginManager, logout_user, UserMixin
@@ -16,6 +16,10 @@ load_dotenv()
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
 class Base(DeclarativeBase):
     pass
 
@@ -26,9 +30,10 @@ class User(UserMixin, db.Model):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(String(320), unique=True, nullable=False)
-    phone: Mapped[str] = mapped_column(String(20), nullable=False)
+    phone: Mapped[str] = mapped_column(String(20))
     password: Mapped[str] = mapped_column(String(255), nullable=False)
     create_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
 
@@ -44,6 +49,10 @@ class Reports(db.Model):
     mimetype: Mapped[str] = mapped_column(String(100), nullable=False)  # Fixed missing type annotation
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
 with app.app_context():
     db.create_all()
 
@@ -52,23 +61,75 @@ with app.app_context():
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    # Returns current year on footer
+    year = datetime.now().strftime("%Y")
 
-@app.route("/login")
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirmPassword")
+
+        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+
+        if user:
+            flash("You've already signed up, please login.")
+            return redirect(url_for("login"))
+
+        if password != confirm_password:
+            flash("Passwords do not match, please try again")
+            return redirect(url_for("register"))
+
+        hashed_and_salted_password = generate_password_hash(
+            password=password,
+            method="pbkdf2:sha256",
+            salt_length=8
+        )
+        new_user = User(
+            first_name=request.form.get("fname"),
+            last_name=request.form.get("lname"),
+            email=email,
+            phone=request.form.get("phone"),
+            password=hashed_and_salted_password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        login_user(new_user)
+        return redirect(url_for("index"))
+    return render_template("register.html", current_user=current_user, year=year)
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    # Returns current year on footer
+    year = datetime.now().strftime("%Y")
+    if request.method == "POST":
+        email = request.form.get("email")
 
-@app.route("dashboard")
+        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        if user:
+            if check_password_hash(user.password, request.form.get("password")):
+                login_user(user)
+                return redirect(url_for("dashboard"))
+            else:
+                flash("Incorrect password, please try again.")
+                return redirect(url_for("login"))
+        else:
+            flash("Email does not exist, please try again")
+            return redirect(url_for("login"))
+
+    return render_template("login.html", current_user=current_user, year=year)
+
+@app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    return "<h1>This is the dashboard</h1>"
 
-@app.route("report_issue")
+@app.route("/report_issue")
 def report_issue():
     return render_template("repost_issue.html")
 
-@app.route("my_report")
+@app.route("/my_report")
 def user_report():
     return render_template("user_report.html")
 
